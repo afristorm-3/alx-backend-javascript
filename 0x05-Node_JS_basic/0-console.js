a41b68aa42f4ed28834a3df924273b76aaf29801
afristorm-3 <afristorm3@gmail.com>
2023-08-23 00:41:20 -0400

    nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Temitope Owolabi <https://github.com/afristorm-3>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
